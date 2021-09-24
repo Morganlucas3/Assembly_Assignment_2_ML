d4d5c3aaf7ad47ced04d707c2d603feb5409140e
MORGAN_LUCAS <LUCAS.MORGAN567@CSU.FULLERTON.EDU>
2021-09-24 13:58:07 -0700

    need to add extern, need to call the assembly file, need to insert the variable name for the integer in the printf statement

--- main.c
+++ main.c
@@ -1,4 +1,24 @@
 
 // TODO: Your code
 
+#include <stdio.h>
+// add extern .asm
+
+int main() {
+
+printf("Welcome to Arrays of Integers\n");
+printf("Brought to you by Morgan Lucas\n");
+
+
+
+// call the assembly program right here
+
+
+printf("Main received this number: %ld\n", //add number right here);
+printf("Main will return 0 to the operating system. Bye!\n");
+
+
+return 0;
+}
+
 
