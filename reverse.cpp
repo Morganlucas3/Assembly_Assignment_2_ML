b7e4a05ecf7891425066828a6592823c8fc8efa2
MORGAN_LUCAS <LUCAS.MORGAN567@CSU.FULLERTON.EDU>
2021-09-27 11:25:23 -0700

    omg

--- reverse.cpp
+++ reverse.cpp
@@ -1,4 +1,47 @@
 
 // TODO: Your code
 
+#include <vector>
+#include <algorithm>
+using namespace std;
 
+extern "C" void reverse(signed long*, long);
+
+void reverse(signed long* a, long b){
+
+
+	
+	//make a long vector
+	vector<long> v;
+	
+	//make a long variable call it temp
+	long temp;
+	
+	//make a for loop (int i = 0; i < b; i++) {
+	//temp = ptr
+	//vector.push_back(temp)
+	// ptr++
+	//}
+	
+	for(int i = 0; i < b; i++){
+	temp = a;
+	v.push_back(temp);
+	a++;
+	}
+		
+	reverse(v.begin(), v.end());
+	
+	//for loop to move ptr (int i = 0; i < b; i--){
+	//ptr--
+	//}
+
+	for(int i = 0; i < b; i--){
+	a--;
+	}
+	
+	for (long i: v){
+	*a = i;
+	a++;
+	}
+
+}
